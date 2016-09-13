d0eb2b3edfeda695adf59b2f01fd597c9fcce0e0
Alex Forbes <alforbes@gumtree.com>
2016-09-13 17:06:24 +0100

    Add initial TestCase

---
+++ tests/test_ascii_art.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+from pysay.ascii_art import py_format
+
+
+class TestAsciiArt(TestCase):
+    def test_py_format_contains_input_text(self):
+        """ Test that py_format output contains the input string
+        """
+        self.assertIn(
+            'test-string',
+            py_format('test-string')
+        )
