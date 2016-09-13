9d201d07919bd48ff34c1b4b1e0726eb5565eaa2
Alex Forbes <alforbes@gumtree.com>
2016-09-13 13:29:04 +0100

    Add main() and parse_args()

--- pysay/__init__.py
+++ pysay/__init__.py
@@ -1,2 +1,16 @@
+from __future__ import print_function
+import argparse
+
 print('pysay imported!')
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('text', help='Text to pysay')
+    return parser.parse_args()
+
+
+def main():
+    print('This is the main function')
+    args = parse_args()
+    print('pysays', args.text)
